a3382f96014bba3836c7f86644fea87796a33224
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-02-12 12:42:41 +0530

    Update host.py

--- host.py
+++ host.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/AOSOFJ/UB-BACK cd /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/AOSOFJ/UB-BACK.git cd /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
